c46c01c1eb81f139b5f530002e8fd0aee287b712
npuNancy <1575812469@qq.com>
2025-03-11 18:41:59 +0800

    feat: 修改训练脚本以使用新的GPU设备

--- downloads/iPersonal_GUI_train/run_stage2_train.sh
+++ downloads/iPersonal_GUI_train/run_stage2_train.sh
@@ -4,14 +4,14 @@
 
 ############## 运行前检查并修改 ##########
 force_torchrun=1    # 是否强制使用 torchrun
-cuda_device="0,1"   # 使用的 GPU 设备
+cuda_device="5,6,7"   # 使用的 GPU 设备
 ############## 运行前检查并修改 ##########
 
 # 模型训练的配置文件路径
 train_config="downloads/iPersonal_GUI_train/qwen25vl_7B_stage2.yaml"
 
 # 保存训练日志的目录
-save_dir="/data4/yanxiaokai/LLaMA-Factory/saves/qwen25vl_7B_stage2/lora"
+save_dir="/data4/yanxiaokai/LLaMA-Factory/saves/qwen25vl_7B_stage2/lora/log"
 
 # 动态生成一个带有时间戳的日志文件名
 log_file="${save_dir}/train_log_$(date +%Y%m%d).txt"
@@ -25,11 +25,9 @@ fi
 
 # 进入  /data4/yanxiaokai/LLaMA-Factory 目录
 cd /data4/yanxiaokai/LLaMA-Factory
-# 激活 llama_factory 环境
-conda activate llama_factory
 
 # 训练
-FORCE_TORCHRUN=${force_torchrun} CUDA_VISIBLE_DEVICES=${cuda_device} lmf train ${train_config} > ${log_file} 2>&1
+FORCE_TORCHRUN=${force_torchrun} CUDA_VISIBLE_DEVICES="$cuda_device" lmf train ${train_config} > ${log_file} 2>&1
 
 # 捕获 lmf 的退出状态
 result=$?
