5eb7abcb91ecbe0fd2f5e05f69329964b94fefa2
nancy <1575812469@qq.com>
2025-03-26 10:05:23 +0800

    feat: 增加每个类别的准确率统计

--- downloads/iPersonal_GUI_eval/eval_stage3.py
+++ downloads/iPersonal_GUI_eval/eval_stage3.py
@@ -27,6 +27,7 @@ def main(filepath, threshold=0.95):
     acc = []
     predict_list = []
     label_list = []
+    category_dict = {}  # 每一个类别的准确率
     # 对每一组数据进行评估
     for item in data:
         try:
@@ -46,6 +47,11 @@ def main(filepath, threshold=0.95):
 
         acc.append(int(predict == label))  # 判断是否相等
 
+        # 统计每个类别的准确率
+        if label not in category_dict:
+            category_dict[label] = []
+        category_dict[label].append(int(predict == label))
+
     # 计算 embeddings
     embeddings_pred = model.encode(predict_list)
     embeddings_true = model.encode(label_list)
@@ -53,10 +59,16 @@ def main(filepath, threshold=0.95):
     similarities_pairwise = model.similarity_pairwise(embeddings_pred, embeddings_true)
 
     # 计算准确率
-    acc = sum(acc) / len(acc) * 100.0
+    accuracy = sum(acc) / len(acc) * 100.0
     sim_acc = (similarities_pairwise > threshold).float().mean().item() * 100.0
-    print(f"Accuracy: {acc:.2f}%")
-    print(f"Similarity Accuracy: {sim_acc:.2f}%")
+    print(f"Acc: {accuracy:.2f}%")
+    print(f"SimAcc: {sim_acc:.2f}%")
+
+    # 输出每个类别的准确率
+    category_acc_dict = {category: sum(value) / len(value) * 100.0 for category, value in category_dict.items()}
+    # 计算平均类别准确率
+    avg_category_acc = np.mean(list(category_acc_dict.values()))
+    print(f"AvgAcc: {avg_category_acc:.2f}%")
 
 
 if __name__ == "__main__":
@@ -65,4 +77,6 @@ if __name__ == "__main__":
     filepath = "saves/qwen25vl_7B_stage3/lora/predict_20way_sft_qwen2.5VL7B/predict_20way_qwen2.5VL7B.jsonl"  # 18.72%
     filepath = "saves/qwen25vl_7B_stage3/lora/predict_20way_sft_gpt-3.5-turbo/generated_predictions.jsonl"  # 7.35%
     filepath = "saves/qwen25vl_7B_stage3/lora/predict_20way_sft_gpt-4o/generated_predictions.jsonl"  # 7.35%
+    filepath = "saves/qwen25vl_7B_stage3/lora/predict_100user_20way_event/generated_predictions.jsonl"  # Acc: 46.9%, AvgAcc: 31.48%
+    filepath = "saves/qwen25vl_7B_stage3/lora/predict_100user_200way_event/generated_predictions.jsonl"  # Acc: 31.2%, AvgAcc: 18.72%
     main(filepath=filepath)
